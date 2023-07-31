46438410b25f5f76893c16bc08116371a3b2ea23
Abhishek Gupta <133193177+ashhek@users.noreply.github.com>
2023-07-31 20:07:32 +0530

    Time: 35 ms (90.69%), Space: 19 MB (22.85%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,15 +1,26 @@
 class Solution {
 private:
-    void dfs(int row, int col, vector<vector<int>>& vis, vector<vector<char>>& grid) {
+    void bfs(int i, int j, vector<vector<int>>& vis, vector<vector<char>>& grid) {
+        queue<pair<int, int>> q;
+        q.push({i, j});
+        vis[i][j] = 1;
+        int offset[] = {0, 1, 0, -1, 0};
         int n = grid.size();
         int m = grid[0].size();
 
-        if (row >= 0 && row < n && col >= 0 && col < m && grid[row][col] == '1' && !vis[row][col]) {
-            vis[row][col] = 1;
-            dfs(row - 1, col, vis, grid);
-            dfs(row + 1, col, vis, grid);
-            dfs(row, col - 1, vis, grid);
-            dfs(row, col + 1, vis, grid);
+        while (!q.empty()) {
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+
+            for (int k = 0; k < 4; ++k) {
+                int nrow = row + offset[k];
+                int ncol = col + offset[k + 1];
+                if (nrow >= 0 && ncol >= 0 && nrow < n && ncol < m && grid[nrow][ncol] == '1' && !vis[nrow][ncol]) {
+                    vis[nrow][ncol] = 1;
+                    q.push({nrow, ncol});
+                }
+            }
         }
     }
 
@@ -19,16 +30,16 @@ public:
         int m = grid[0].size();
 
         vector<vector<int>> vis(n, vector<int>(m, 0));
-        int count = 0;
+        int ans = 0;
 
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < m; ++j) {
                 if (grid[i][j] == '1' && !vis[i][j]) {
-                    count++;
-                    dfs(i, j, vis, grid);
+                    ans++;
+                    bfs(i, j, vis, grid);
                 }
             }
         }
-        return count;
+        return ans;
     }
 };
