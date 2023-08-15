34f8b172f82bd43b95c41e8aa830505d50f8e71f
Abhishek Gupta <133193177+ashhek@users.noreply.github.com>
2023-08-15 19:12:41 +0530

    Added solution - LeetHub

--- Implementing Dijkstra Algorithm - GFG/implementing-dijkstra-algorithm.cpp
+++ Implementing Dijkstra Algorithm - GFG/implementing-dijkstra-algorithm.cpp
@@ -12,25 +12,25 @@ class Solution
     {
         // Code here
         set<pair<int,int>> st;
-        vector<int> dist(V, 1e9);
-        st.insert({0, S});
+        vector<int>dist(V, 1e9);
         dist[S] = 0;
-         
+        st.insert({0, S});
+        
         while(!st.empty()){
             auto it = *(st.begin());
             int dis = it.first;
             int node = it.second;
-            st.erase(it);
+            st.erase({dis, node});
             
             for(auto it:adj[node]){
-                int edgeWeight = it[1];
+                int edgeW = it[1];
                 int adjNode = it[0];
                 
-                if(dis + edgeWeight < dist[adjNode]){
-                    if(dist[adjNode] != 1e9){
-                        st.erase({dist[adjNode], adjNode});
-                    }
-                    dist[adjNode] = dis + edgeWeight;
+                if(dis + edgeW < dist[adjNode]){
+                    if(dist[adjNode] != 1e9)
+                    st.erase({dist[adjNode], adjNode});
+                    
+                    dist[adjNode] = dis + edgeW;
                     st.insert({dist[adjNode], adjNode});
                 }
             }
