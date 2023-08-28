fb3c8e2272f3222df9e027b944bdcd27bea9bbea
Abhishek Gupta <133193177+ashhek@users.noreply.github.com>
2023-08-28 23:45:18 +0530

    Time: 5 ms (26.96%), Space: 6.3 MB (26.09%) - LeetHub

--- 2729-check-if-the-number-is-fascinating/2729-check-if-the-number-is-fascinating.cpp
+++ 2729-check-if-the-number-is-fascinating/2729-check-if-the-number-is-fascinating.cpp
@@ -1,9 +1,16 @@
 class Solution {
 public:
     bool isFascinating(int n) {
-    string s = to_string(n) + to_string(n*2) + to_string(n*3);
-    unordered_set<char> st(s.begin(), s.end());
-    return (st.size() == 9 && s.size() == 9 && st.find('0') == st.end() );
-
+        unordered_set<int> m;
+        int t=0;
+        for (int i=1;i<4;i++){
+            t = n*i;
+            while (t>0) {
+                if (t%10==0 || m.find(t%10)!=m.end()) return false;
+                m.insert(t%10);
+                t /=10;
+            }
+        }
+        return true;
     }
 };
