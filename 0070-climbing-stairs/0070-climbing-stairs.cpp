5d9c44a79ce4268ac768b1abfc13528f1f58b102
Abhishek Gupta <133193177+ashhek@users.noreply.github.com>
2023-09-14 20:45:30 +0530

    Time: 2 ms (45.77%), Space: 6.7 MB (6.11%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
-   
     int climbStairs(int n) {
+        vector<int>dp(n+1, -1);
 
-     int N = 1e5+10;
-     int dp[N];
-    
-     memset(dp, -1, sizeof(dp));
-     return fib(n,dp);
-    }
+        dp[0] = 1;
+        dp[1] = 1;
 
-    int fib(int n, int dp[]){
-
-        if(n<=2) return n;
-        if(dp[n] != -1) return dp[n];
-        
-        return dp[n] = fib(n-1,dp)+fib(n-2,dp);
+        for(int i=2; i<=n; ++i){
+            dp[i] = dp[i-2]+dp[i-1];
+        }
+        return dp[n];
     }
 };
