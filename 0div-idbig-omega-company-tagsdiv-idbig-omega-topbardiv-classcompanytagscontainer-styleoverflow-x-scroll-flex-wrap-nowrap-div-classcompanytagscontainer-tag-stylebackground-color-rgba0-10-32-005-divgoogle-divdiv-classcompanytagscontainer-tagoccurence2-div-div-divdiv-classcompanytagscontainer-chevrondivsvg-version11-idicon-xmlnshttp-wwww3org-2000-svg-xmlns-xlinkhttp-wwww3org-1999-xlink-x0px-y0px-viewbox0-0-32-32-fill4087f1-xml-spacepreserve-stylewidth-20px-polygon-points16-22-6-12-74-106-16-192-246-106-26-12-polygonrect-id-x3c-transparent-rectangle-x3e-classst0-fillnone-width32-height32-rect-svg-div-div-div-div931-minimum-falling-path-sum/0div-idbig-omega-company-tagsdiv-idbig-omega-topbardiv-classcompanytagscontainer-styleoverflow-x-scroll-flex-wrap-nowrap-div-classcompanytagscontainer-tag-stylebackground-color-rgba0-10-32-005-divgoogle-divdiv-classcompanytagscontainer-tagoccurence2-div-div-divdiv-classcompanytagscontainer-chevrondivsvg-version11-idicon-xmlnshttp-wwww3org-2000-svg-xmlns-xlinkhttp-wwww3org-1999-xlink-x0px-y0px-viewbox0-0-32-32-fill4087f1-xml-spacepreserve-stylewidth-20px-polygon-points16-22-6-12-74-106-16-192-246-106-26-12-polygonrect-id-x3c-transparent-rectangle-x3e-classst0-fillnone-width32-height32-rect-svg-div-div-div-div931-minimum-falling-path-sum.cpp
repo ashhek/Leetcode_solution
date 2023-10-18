4a8de7e879d1e9f8f4baa7b3741368e30589ab0b
Abhishek Gupta <133193177+ashhek@users.noreply.github.com>
2023-10-18 13:20:23 +0530

    Time: 11 ms (77.65%), Space: 10.9 MB (30.83%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div931-minimum-falling-path-sum/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div931-minimum-falling-path-sum.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div931-minimum-falling-path-sum/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div931-minimum-falling-path-sum.cpp
@@ -1,43 +1,25 @@
 class Solution {
-    // int f(int i, int j, vector<vector<int>>& matrix, vector<vector<int>>& dp, int m){       
-    //     if(j<0 or j>=matrix[0].size()) return 1e9;  
-    //     if(i==0) return matrix[i][j];
-        
+    int f(int i, int j, vector<vector<int>>& matrix, vector<vector<int>>& dp, int m){
+        if(i==0) return matrix[i][j];
+        if(i<0 or j<0) return 1e9;
 
-    //     if(dp[i][j] != -1) return dp[i][j];
+        if(dp[i][j] != 1e9) return dp[i][j];
         
-    //     int up = matrix[i][j] + f(i-1, j, matrix, dp, m);
-    //     int left = matrix[i][j] + f(i-1, j-1, matrix, dp, m);
-    //     int right = matrix[i][j] + f(i-1, j+1, matrix, dp, m);
+        int up = 1e9, left = 1e9, right = 1e9;
+        if(i>0) up = matrix[i][j] + f(i-1, j, matrix, dp, m);
+        if(i>0 and j>0) left = matrix[i][j] + f(i-1, j-1, matrix, dp, m);
+        if(i>0 and j<m-1) right = matrix[i][j] + f(i-1, j+1, matrix, dp, m);
 
-    //     return dp[i][j] = min({up, left, right});
-    // }
+        return dp[i][j] = min({up, left, right});
+    }
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int m = matrix[0].size();
         int n = matrix.size();
-        vector<vector<int>>dp(n, vector<int>(m, -0));
-
-        for(int j=0; j<m; ++j) dp[0][j] = matrix[0][j];
-
-        for(int i=1; i<n; ++i){
-            for(int j=0; j<m; ++j){
-                
-                int up = matrix[i][j] + dp[i-1][j];
-                int left = 1e9;
-                if(j-1>=0) left = matrix[i][j] + dp[i-1][j-1];
-    
-                int right = 1e9;
-                if(j+1<m) right = matrix[i][j] + dp[i-1][j+1];
-    
-
-                dp[i][j] = min({up, left, right});
-            }
-        }
-
-        int cost = 1e9;
+        vector<vector<int>>dp(n, vector<int>(m, 1e9));
+        int cost = INT_MAX;
         for(int j=0; j<m; ++j){
-            cost = min(cost, dp[n-1][j]);
+            cost = min(cost, f(n-1, j, matrix, dp, m));
         }
         return cost;
     }
