0a813500862281fcde7bb2e9a956a6c7667490fb
Abhishek Gupta <133193177+ashhek@users.noreply.github.com>
2023-10-25 11:38:21 +0530

    Added solution - LeetHub

--- Partitions with Given Difference - GFG/partitions-with-given-difference.cpp
+++ Partitions with Given Difference - GFG/partitions-with-given-difference.cpp
@@ -4,35 +4,47 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution {
-    int mod = (int)1e9+7;
+    int mod = (int)(1e9+7);
+  private:
+  int f(int i, int target, vector<int>& arr,vector<vector<int>>& dp){
+      if(i<0) return target==0;
+      if(dp[i][target] != -1) return dp[i][target];
+      int notPick = f(i-1, target, arr, dp);
+      int pick = 0;
+      if(arr[i]<=target) pick = f(i-1, target-arr[i], arr, dp);
+      
+      return dp[i][target] = (pick + notPick)%mod;
+  }
   public:
-    int countPartitions(int n, int target, vector<int>& nums) {
-        // Code here
-       // int n = nums.size();
-        int sum = 0; for(auto it:nums) sum += it;
-
-        if(sum-target<0) return 0;
-        if((sum-target) % 2) return 0;
-
-        int k = (sum-target)/2;
-
-        vector<vector<int>>dp(n, vector<int>(k+1, 0));
-
-        if(nums[0]==0) dp[0][0] = 2;
-        else dp[0][0] = 1;
-
-        if(nums[0]!=0 and nums[0]<=k) dp[0][nums[0]] = 1;
-
-        for(int i=1; i<n; ++i){
-            for(int target=0; target<=k; ++target){
-                int notPick = dp[i-1][target];
-                int pick = 0;
-                if(nums[i]<=target) pick = dp[i-1][target-nums[i]];
-
-                dp[i][target] = (pick+notPick)%mod;
-            }
-        }
-        return dp[n-1][k];
+    int countPartitions(int n, int d, vector<int>& arr) {
+    int totSum = 0;
+    for(int i=0; i<n;i++){
+        totSum += arr[i];
+    }
+    if(totSum-d<0) return 0;
+    if((totSum-d)%2==1) return 0;
+    
+    int s2 = (totSum-d)/2;
+    
+    vector<vector<int>> dp(n,vector<int>(s2+1, 0));
+    //return f(n-1,s2,arr, dp);
+    
+    
+      if(arr[0]==0) dp[0][0] = 2;
+      else dp[0][0] = 1;
+      
+      if(arr[0] != 0 and s2>=arr[0]) dp[0][arr[0]] = 1;
+      
+      for(int i=1; i<n; ++i){
+          for(int target = 0; target<=s2; ++target){
+              int notPick = dp[i-1][target];
+              int pick = 0;
+              if(target>=arr[i]) pick = dp[i-1][target-arr[i]];
+              
+              dp[i][target] = (pick + notPick)%mod;
+          }
+      }
+      return dp[n-1][s2];
     }
 };
 
