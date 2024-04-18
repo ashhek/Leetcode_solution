96c0381e14d9366aa6c2455caf012119a4453dc2
Abhishek Gupta <133193177+ashhek@users.noreply.github.com>
2024-04-18 14:22:18 +0530

    Time: 85 ms (26.25%), Space: 100.8 MB (76.64%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -1,32 +1,18 @@
 class Solution {
 public:
-void dfs(int row,int col,vector<vector<int>>&vis,vector<vector<int>>& grid,int& cnt){
-    vis[row][col]=1;
-    int a=0;
-    int drow[4]={1,-1,0,0};
-    int dcol[4]={0,0,1,-1};
-    for(int i=0;i<4;i++){
-          int nrow=row+drow[i];
-          int ncol=col+dcol[i];
-          if(nrow>=0 && ncol>=0 && nrow<grid.size() && ncol<grid[0].size() && grid[nrow][ncol]==1){
-              a++;
-          }
-          if(nrow>=0 && ncol>=0 && nrow<grid.size() && ncol<grid[0].size() && grid[nrow][ncol]==1&& vis[nrow][ncol]==0 ){
-          
-              dfs(nrow,ncol,vis,grid,cnt);
-          }
-    }
-    cnt+=4-a;
-    }
     int islandPerimeter(vector<vector<int>>& grid) {
-        vector<vector<int>>vis(grid.size(),vector<int>(grid[0].size(),0));
-        int cnt=0;
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
-                if(!vis[i][j] && grid[i][j]==1)
-                       dfs(i,j,vis,grid,cnt);
+        int ans=0;
+        int n=grid.size();
+        int m=grid[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1){
+                    ans+=4;
+                    if(i>0&&grid[i-1][j]==1) ans-=2;
+                    if(j>0&&grid[i][j-1]==1) ans-=2;
+                }
             }
         }
-        return cnt;
+        return ans;
     }
 };
