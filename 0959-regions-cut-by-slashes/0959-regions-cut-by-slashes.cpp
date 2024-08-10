633fda5b701b5a8e5bcdf5658028d3014f8096a6
Abhishek Gupta <133193177+ashhek@users.noreply.github.com>
2024-08-10 22:09:09 +0530

    Time: 7 ms (64.5%), Space: 11.7 MB (81.3%) - LeetHub

--- 0959-regions-cut-by-slashes/0959-regions-cut-by-slashes.cpp
+++ 0959-regions-cut-by-slashes/0959-regions-cut-by-slashes.cpp
@@ -1,26 +1,85 @@
 class Solution {
-public:
-    int dfs(vector<vector<int>>& g, int i, int j) {
-        if (min(i, j) < 0 || max(i, j) >= g.size() || g[i][j] != 0)
-            return 0;
-        g[i][j] = 1;
-        return 1 + dfs(g, i - 1, j) + dfs(g, i + 1, j) + dfs(g, i, j - 1) +
-               dfs(g, i, j + 1);
+
+    vector<int> parent;
+
+    int getParent(int node) {
+        if (parent[node] == node) {
+            return node;
+        }
+
+        return parent[node] = getParent(parent[node]);
     }
+
+    void unionSet(int a, int b) {
+        int parentA = getParent(a);
+        int parentB = getParent(b);
+
+        if (parentA == parentB) {
+            return;
+        }
+
+        parent[parentB] = parentA;
+    }
+
+public:
     int regionsBySlashes(vector<string>& grid) {
-        int n = grid.size(), regions = 0;
-        vector<vector<int>> g(n * 3, vector<int>(n * 3, 0));
-        for (int i = 0; i < n; ++i)
-            for (int j = 0; j < n; ++j)
-                if (grid[i][j] == '/')
-                    g[i * 3][j * 3 + 2] = g[i * 3 + 1][j * 3 + 1] =
-                        g[i * 3 + 2][j * 3] = 1;
-                else if (grid[i][j] == '\\')
-                    g[i * 3][j * 3] = g[i * 3 + 1][j * 3 + 1] =
-                        g[i * 3 + 2][j * 3 + 2] = 1;
-        for (int i = 0; i < n * 3; ++i)
-            for (int j = 0; j < n * 3; ++j)
-                regions += dfs(g, i, j) ? 1 : 0;
-        return regions;
+        int n = grid.size();
+        if (n == 0) {
+            return 0;
+        }
+        int m = grid[0].size();
+        parent = vector<int>((n + 1) * (m + 1) + 1);
+        for (int i = 0; i < parent.size(); i++) {
+            parent[i] = i;
+        }
+
+        for (int i = 0; i < n; i++) {
+            int a = i * (m + 1);
+            int b = (i + 1) * (m + 1);
+            unionSet(a, b);
+
+            a = i * (m + 1) + m;
+            b = (i + 1) * (m + 1) + m;
+            unionSet(a, b);
+        }
+
+        for (int j = 0; j < m; j++) {
+            int a = j;
+            int b = j + 1;
+            unionSet(a, b);
+
+            a = (n) * (m + 1) + j;
+            b = (n) * (m + 1) + j + 1;
+            unionSet(a, b);
+        }
+
+        int cnt = 1;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                int end1;
+                int end2;
+                if (grid[i][j] == '/') {
+                    // connect (i, j+1) and (i+1, j)
+                    end1 = i * (m + 1) + j + 1;
+                    end2 = (i + 1) * (m + 1) + j;
+                } else if (grid[i][j] == '\\') {
+                    // connect (i, j) and (i+1, j+1)
+                    end1 = i * (m + 1) + j;
+                    end2 = (i + 1) * (m + 1) + j + 1;
+                } else {
+                    continue;
+                }
+                int parentA = getParent(end1);
+                int parentB = getParent(end2);
+                if (parentA == parentB) {
+                    cnt++;
+                } else {
+                    parent[parentB] = parentA;
+                }
+            }
+        }
+
+        return cnt;
     }
 };
