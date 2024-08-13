cd4d09e91cec98e679575590fd6166fce487fec3
Abhishek Gupta <133193177+ashhek@users.noreply.github.com>
2024-08-13 14:08:34 +0530

    Time: 47 ms (5.19%), Space: 26 MB (6.16%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,27 +1,35 @@
 class Solution {
-public:
-     set<vector<int>>s;
-    void helper(vector<int>&nums, vector<int>&temp, int target, int currsum, int i){
-        if(currsum>target) return;
-        if(currsum==target){
-            s.insert(temp);
+    void uniqCandi(int i, vector<int>& candidates, int target,
+                   set<vector<int>>& st, vector<int> temp) {
+        if (target == 0) {
+            st.insert(temp);
+            return;
         }
-        if(i==nums.size()) return;
-
-        currsum += nums[i];
-        temp.push_back(nums[i]);
-        helper(nums, temp, target, currsum, i+1);
-
-        currsum -= nums[i];
-        temp.pop_back();
-        while(i+1<nums.size() && nums[i]==nums[i+1]) i++;
-        helper(nums, temp, target, currsum, i+1);
+        if (i == candidates.size()) {
+            return;
+        }
+        if (candidates[i] <= target) {
+            temp.push_back(candidates[i]);
+            uniqCandi(i + 1, candidates, target - candidates[i], st, temp);
+            temp.pop_back();
+        }
+        while(i+1 < candidates.size()) {
+            if(candidates[i] == candidates[i+1]) 
+            i++;
+            else break;
+        }
+        return uniqCandi(i + 1, candidates, target, st, temp);
     }
+
+public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        set<vector<int>> st;
         vector<int> temp;
-        sort(candidates.begin(),candidates.end());
-        helper(candidates, temp, target, 0, 0);
-        vector<vector<int>>ans(s.begin(),s.end());
+        sort(candidates.begin(), candidates.end());
+
+        uniqCandi(0, candidates, target, st, temp);
+
+        vector<vector<int>> ans(st.begin(), st.end());
         return ans;
     }
 };
