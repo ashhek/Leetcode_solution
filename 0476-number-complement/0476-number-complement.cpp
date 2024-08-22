417691607b5a5905c9bb49eac277601ca201575d
Abhishek Gupta <133193177+ashhek@users.noreply.github.com>
2024-08-22 12:59:35 +0530

    Time: 0 ms (100%), Space: 7.5 MB (33.08%) - LeetHub

--- 0476-number-complement/0476-number-complement.cpp
+++ 0476-number-complement/0476-number-complement.cpp
@@ -1,11 +1,30 @@
 class Solution {
 public:
     int findComplement(int num) {
-        int n = (int)log2(num) + 1;
+        // method-1
+        //int n = (int)log2(num) + 1;
 
-        for(int i=0; i<n; ++i) {
-            num ^= (1<<i);
-        }
-        return num;
+        // for (int i = 0; i < n; ++i) {
+        //     num ^= (1 << i);
+        // }
+       // return num;
+        // method - 2
+
+         //int n = (int)log2(num) + 1;
+        // unsigned mask = (1U<<n) - 1;
+        // return mask ^ num;
+
+        //method-3
+        int i = 0;
+        int complement = 0;
+
+        while(num) {
+            if(!(num & 1)) {
+                complement |= (1<<i);
+            }
+            num >>= 1;
+            i++;
+        } 
+        return complement;
     }
 };
