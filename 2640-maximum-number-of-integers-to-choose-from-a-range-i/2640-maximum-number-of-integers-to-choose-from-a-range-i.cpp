3df1bc32666affd4501283381ca4a8388cae88ca
Abhishek Gupta <133193177+ashhek@users.noreply.github.com>
2024-12-06 10:16:20 +0530

    Time: 3 ms (99.62%), Space: 122.3 MB (74.57%) - LeetHub

--- 2640-maximum-number-of-integers-to-choose-from-a-range-i/2640-maximum-number-of-integers-to-choose-from-a-range-i.cpp
+++ 2640-maximum-number-of-integers-to-choose-from-a-range-i/2640-maximum-number-of-integers-to-choose-from-a-range-i.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     int maxCount(vector<int>& banned, int n, int maxSum) {
-        set<int>st(banned.begin(), banned.end());
-
+        vector<int>vis(n+1, -1);
+        for(int i=0; i<banned.size(); ++i) {
+            if(banned[i] <= n)
+            vis[banned[i]] = 0;
+        }
         int cnt = 0;
         int sum = 0;
-        for(int i = 1; i<=n; ++i) {
-            if(st.find(i) == st.end()) {
-                if(sum + i > maxSum) {
+        for (int i = 1; i <= n; ++i) {
+            if (vis[i] == -1) {
+                if (sum + i > maxSum) {
                     break;
                 }
                 cnt++;
