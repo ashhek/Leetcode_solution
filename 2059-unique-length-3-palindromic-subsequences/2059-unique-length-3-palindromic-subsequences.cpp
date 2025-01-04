4c5dd7a20dee8a1b5e1c3b09044585a406785a2e
Abhishek Gupta <133193177+ashhek@users.noreply.github.com>
2025-01-04 14:56:49 +0530

    Time: 144 ms (84.05%), Space: 15.9 MB (69.02%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
     int countPalindromicSubsequence(string s) {
-          int first[26] = {[0 ... 25] = INT_MAX}, last[26] = {}, res = 0;
-    for (int i = 0; i < s.size(); ++i) {
-        first[s[i] - 'a'] = min(first[s[i] - 'a'], i);
-        last[s[i] - 'a'] = i;
-    }
-    for (int i = 0; i < 26; ++i)
-        if (first[i] < last[i])
-            res += unordered_set<char>(begin(s) + first[i] + 1, begin(s) + last[i]).size();
-    return res;
+        int n = s.size();
+        vector<int>first(26, INT_MAX);
+        int last[26] = {};
+        for (int i = 0; i < n; ++i) {
+            first[s[i] - 'a'] = min(first[s[i]-'a'], i);
+            last[s[i] - 'a'] = i;
+        }
+        int cnt = 0;
+        for (int i = 0; i < 26; ++i) {
+            if (first[i] < last[i]) {
+                unordered_set<char> st(s.begin() + first[i] + 1,
+                                   s.begin() + last[i]);
+                cnt += st.size();
+            }
+        }
+        return cnt;
     }
 };
