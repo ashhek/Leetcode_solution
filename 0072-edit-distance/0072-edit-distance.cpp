73af3c84b192e1e1e1a965b8a51dc436e618b693
Abhishek Gupta <133193177+ashhek@users.noreply.github.com>
2025-01-05 23:31:45 +0530

    Time: 20 ms (19.66%), Space: 14.1 MB (18.87%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,32 +1,46 @@
 class Solution {
     int dp[501][501];
-    int help(int i, int j, string& s, string &t) {
-        if(i < 0) return j;
-        if(j < 0) return i;
-        
-        if(dp[i][j] != -1) return dp[i][j];
+    int help(int i, int j, string& s, string& t) {
+        if (i == 0)
+            return j;
+        if (j == 0)
+            return i;
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
 
         int opr = INT_MAX;
-        int insert = INT_MAX, del = INT_MAX, rep = INT_MAX;
-        if(s[i] == t[j]) {
-            opr = min(opr, help(i-1, j-1, s, t));
-        } else {
-            insert = 1 + help(i, j-1, s, t);
-            del = 1 + help(i-1, j, s, t);
-            rep = 1 + help(i-1, j-1, s, t);
-        }
-        return dp[i][j] = min({opr, insert, del, rep});
-    }
-public:
-    int minDistance(string word1, string word2) {
-        int n = word1.size();
-        int m = word2.size();
-        if(n == 0) return m;
-        if(m == 0) return n;
-        memset(dp, -1, sizeof(dp));
-        int x = help(word1.size()-1, word2.size()-1, word1, word2);
+        if (s[i - 1] == t[j - 1])
+            return dp[i][j] = min(opr, help(i - 1, j - 1, s, t));
 
-        return x+1;
-        
+        return dp[i][j] = 1 + min({help(i, j - 1, s, t), help(i - 1, j, s, t),help(i - 1, j - 1, s, t)});
+    }
+
+public:
+    int minDistance(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+        // memset(dp, -1, sizeof(dp));
+        // return help(n, m, word1, word2);
+
+        vector<vector<int>>dp(n+1, vector<int>(m+1, 0));
+        for(int i=0; i<=n; ++i) {
+            dp[i][0] = i;
+        }
+        for(int j=0; j<=m; ++j) {
+            dp[0][j] = j;
+        }
+        for(int i=1; i<=n; ++i) {
+            for(int j=1; j<=m; ++j) {
+                int opr = INT_MAX;
+
+                if(s[i-1] == t[j-1]) {
+                    dp[i][j] = min(opr, dp[i-1][j-1]);
+                } else {
+                    dp[i][j] = 1 + min({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]});
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
