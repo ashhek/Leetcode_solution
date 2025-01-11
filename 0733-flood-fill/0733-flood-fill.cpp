30c206ffea8b6bd920185b23b9da22ae22c071e9
Abhishek Gupta <133193177+ashhek@users.noreply.github.com>
2025-01-12 01:06:56 +0530

    Time: 0 ms (100%), Space: 18.7 MB (9.04%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,20 +1,39 @@
 class Solution {
-    void dfs(int r, int c, vector<vector<int>>& image, int ic, int fc) {
-        if(r<0 || r>=image.size() || c<0 || c>=image[0].size()) return;
-        if(image[r][c] != ic) return;
-
-        image[r][c] = fc;
-
-        dfs(r-1, c, image, ic, fc);
-        dfs(r+1, c, image, ic, fc);
-        dfs(r, c-1, image, ic, fc);
-        dfs(r, c+1, image, ic, fc);
-    }
 public:
+    void bfs(int i, int j, vector<vector<int>>&image, vector<vector<int>>&vis, int color) {
+        queue<pair<int,int>>q;
+        q.push({i, j});
+        int initColor = image[i][j];
+        vis[i][j] = 1;
+
+        int xdir[] = {0, 0, 1, -1};
+        int ydir[] = {1, -1, 0, 0};
+
+        while(!q.empty()) {
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+
+            image[row][col] = color;
+
+            for(int k=0; k<4; ++k) {
+                int nr = row + xdir[k];
+                int nc = col + ydir[k];
+
+                if(nr>=0 && nr<image.size() && nc>=0 && nc<image[0].size() && !vis[nr][nc] && image[nr][nc] == initColor) {
+                    vis[nr][nc] = 1;
+                    q.push({nr, nc});
+                }
+            }
+
+        }
+    }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int ic = image[sr][sc];
-        if(ic != color)
-        dfs(sr, sc, image, ic, color);
+        int n = image.size();
+        int m = image[0].size();
+
+        vector<vector<int>>vis(n, vector<int>(m, 0));
+        bfs(sr, sc, image, vis, color);
         return image;
     }
 };
