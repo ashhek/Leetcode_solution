a1119800fcabe354769cdfb0bc606bad6a8c4a83
Abhishek Gupta <133193177+ashhek@users.noreply.github.com>
2025-01-17 01:18:02 +0530

    Time: 0 ms (100%), Space: 9.8 MB (78.38%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,31 +1,27 @@
 class Solution {
-    bool areVectorsEqual(vector<int> a, vector<int> b){
-        for(int i=0; i<26; i++){
-            if(a[i]!=b[i]) return false;
-        }
-        return true;
-    }
 public:
     bool checkInclusion(string s1, string s2) {
-        if(s2.size()<s1.size()) return false;
-        vector<int> freqS1(26, 0);
-        for(char c: s1) freqS1[c-'a']++;
-        
-        vector<int> freqS2(26, 0);
-        int i=0, j=0;
-        
-        while(j<s2.size()){
-            freqS2[s2[j]-'a']++;
-            
-            if(j-i+1==s1.size()){
-                if(areVectorsEqual(freqS1, freqS2)) return true;
+        int n = s1.size();
+        int m = s2.size();
+
+        vector<int>cnt(26, 0);
+        for(char ch : s1) cnt[ch-'a']++;
+        int left = 0;
+        for(int right=0; right<m; ++right) {
+            if(right >= n) {
+                cnt[s2[left]-'a']++;
+                left++;
             }
-            
-            if(j-i+1<s1.size()) j++;
-            else{
-                freqS2[s2[i]-'a']--;
-                i++;
-                j++;
+            cnt[s2[right]-'a']--;
+            if(right >= n-1) {
+                bool found = false;
+                for(int i=0; i<26; ++i) {
+                    if(cnt[i]) {
+                        found = true;
+                        break;
+                    }
+                }
+                if(!found) return true;
             }
         }
         return false;
