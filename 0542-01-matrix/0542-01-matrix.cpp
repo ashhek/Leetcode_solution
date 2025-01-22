f4989dc11f6334dfdb322b08e9b98206815eb418
Abhishek Gupta <133193177+ashhek@users.noreply.github.com>
2025-01-22 11:45:01 +0530

    Time: 13 ms (80.24%), Space: 37.2 MB (56.95%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -3,37 +3,35 @@ public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int n = mat.size();
         int m = mat[0].size();
-        int offset[5] = {0, 1, 0, -1, 0};
-        vector<vector<int>> dist(n, vector<int>(m,0));
 
-        vector<vector<int>> vis(n, vector<int>(m,0));
-        queue<pair<pair<int,int>,int>>q;
-
-
-        for(int i=0; i<n; ++i){
-            for(int j=0; j<m; ++j){
-                if(mat[i][j]==0){
-                    q.push({{i,j}, 0});
+        vector<vector<int>>vis(n, vector<int>(m, 0));
+        queue<pair<int,int>>q;
+        for(int i=0; i<n; ++i) {
+            for(int j=0; j<m; ++j) {
+                if(mat[i][j] == 0) {
+                    q.push({i, j});
                     vis[i][j] = 1;
-                }
+                } 
             }
         }
-        while(!q.empty()){
-            int row = q.front().first.first;
-            int col = q.front().first.second;
-            int steps = q.front().second;
+        int dir[] = {0, -1, 0, 1, 0};
+        while(!q.empty()) {
+            int row = q.front().first;
+            int col = q.front().second;
             q.pop();
-            dist[row][col] = steps;
 
-            for(int i=0; i<4; ++i){
-                int nrow = row + offset[i];
-                int ncol = col + offset[i+1];
-                if(nrow>=0 and ncol>=0 and nrow<n and ncol<m and !vis[nrow][ncol]){
+            for(int k=0; k<4; ++k) {
+                int nrow = row + dir[k];
+                int ncol = col + dir[k+1];
+
+                if(nrow >=0 && nrow < n && ncol >= 0 
+                   && ncol < m && !vis[nrow][ncol]) {
                     vis[nrow][ncol] = 1;
-                    q.push({{nrow,ncol}, steps+1});
+                    mat[nrow][ncol] = mat[row][col] + 1;
+                    q.push({nrow, ncol});
                 }
             }
         }
-        return dist;
+        return mat;
     }
 };
