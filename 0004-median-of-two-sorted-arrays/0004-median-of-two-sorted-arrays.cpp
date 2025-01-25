de29996297147e37d23a4998e0717a6de0f331ee
Abhishek Gupta <133193177+ashhek@users.noreply.github.com>
2025-01-25 19:07:21 +0530

    Time: 0 ms (100%), Space: 97.8 MB (11.42%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,42 +1,42 @@
 class Solution {
-    int findKthelement(vector<int>&nums1, vector<int>&nums2, int n, int m, int k){
-        if(n > m) return findKthelement(nums2, nums1, m, n, k);
-
+    int findKth(vector<int>nums1, vector<int>nums2, int n, int m, int k) {
+        if(n > m) {
+            return findKth(nums2, nums1, m, n, k);
+        }
         int low = max(0, k-m);
         int high = min(k, n);
-        int left = k;
 
-        while(low <= high){
+        while(low <= high) {
             int mid1 = (low + high) >> 1;
-            int mid2 = left - mid1;
+            int mid2 = k - mid1;
 
-            int l1 = INT_MIN, l2 = INT_MIN;
-            int r1 = INT_MAX, r2 = INT_MAX;
+            int l1 = mid1 > 0 ? nums1[mid1-1] : INT_MIN;
+            int l2 = mid2 > 0 ? nums2[mid2-1] : INT_MIN;
+            int r1 = mid1 < n ? nums1[mid1] : INT_MAX;
+            int r2 = mid2 < m ? nums2[mid2] : INT_MAX;
 
-            if(mid1 > 0) l1 = nums1[mid1-1];
-            if(mid2 > 0) l2 = nums2[mid2-1];
-            if(mid1 < n) r1 = nums1[mid1];
-            if(mid2 < m) r2 = nums2[mid2];
-
-            if(l1 <= r2 && l2 <= r1){
+            if(l1 <= r2 && l2 <= r1) {
                 return max(l1, l2);
-            } else if(l1 > r2){
+            } else if(l1 > r2) {
                 high = mid1 - 1;
             } else {
                 low = mid1 + 1;
             }
-        }
-        return -1;
+       }
+       return -1;
     }
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size();
         int m = nums2.size();
 
-        int x = n+m;
-        if(x & 1){
-            return (double)findKthelement(nums1, nums2, n, m, x/2 + 1);
-        } 
-        return ((double)findKthelement(nums1, nums2, n, m, x/2) + (double)findKthelement(nums1, nums2, n, m, x/2 + 1))/2.0;
+        int x = n + m;
+
+        double first = (double)findKth(nums1, nums2, n, m, x/2+1);
+        double second = (double)findKth(nums1, nums2, n, m, x/2);
+        if(x & 1) {
+            return first;
+        }
+        return (first+second)/2.0;
     }
 };
