cf50e3fd458679ef217692dd5bcb55401098d124
Abhishek Gupta <133193177+ashhek@users.noreply.github.com>
2025-01-26 22:51:35 +0530

    Time: 242 ms (5.24%), Space: 353.3 MB (5.69%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -1,31 +1,49 @@
-#include <vector>
-using namespace std;
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
-public:
-    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        if (!l1) return l2;
-        if (!l2) return l1;
-
-        if (l1->val < l2->val) {
-            l1->next = mergeTwoLists(l1->next, l2);
-            return l1;
-        } else {
-            l2->next = mergeTwoLists(l1, l2->next);
-            return l2;
+private:
+    ListNode* conquer(ListNode* left, ListNode* right) {
+        ListNode* dummy = new ListNode(-1);
+        ListNode* curr = dummy;
+        while(left && right) {
+            if(left->val < right->val) {
+                curr->next = left;
+                left = left->next;
+            } else {
+                curr->next = right;
+                right = right->next;
+            }
+            curr = curr->next;
         }
+        if(left) {
+            curr->next = left;
+        } else {
+            curr->next = right;
+        }
+        return dummy->next;
     }
+    ListNode* divide(vector<ListNode*>lists, int low, int high) {
+        if(low > high) return nullptr;
+        if(low == high) return lists[low];
 
+        int mid = (low + high) >> 1;
+        ListNode* left = divide(lists, low, mid);
+        ListNode* right = divide(lists, mid+1, high);
+        return conquer(left, right);
+    }    
+public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        if (lists.empty()) return nullptr;
-        return divideAndConquer(lists, 0, lists.size() - 1);
-    }
-
-    ListNode* divideAndConquer(vector<ListNode*>& lists, int left, int right) {
-        if (left == right) return lists[left];
-
-        int mid = left + (right - left) / 2;
-        ListNode* l1 = divideAndConquer(lists, left, mid);
-        ListNode* l2 = divideAndConquer(lists, mid + 1, right);
-        return mergeTwoLists(l1, l2);
+        if(lists.empty()) {
+            return nullptr;
+        }
+        return divide(lists, 0, lists.size()-1);
     }
 };
