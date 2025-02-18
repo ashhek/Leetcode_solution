d46d3be15eeabf13719f4d68f8551a050f25bf12
Abhishek Gupta <133193177+ashhek@users.noreply.github.com>
2025-02-18 13:57:31 +0530

    Time: 0 ms (100%), Space: 8 MB (57.81%) - LeetHub

--- 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.cpp
+++ 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.cpp
@@ -1,54 +1,18 @@
 class Solution {
-    bool backTrack(int idx, string& s, string& res, vector<int>& seen,
-                   char prev) {
-        if (res.size() == s.size()) {
-            return true;
-        }
+public:
+    string smallestNumber(string pattern) {
+        string result;
+        stack<char> st;
 
-        //cout << "idx: " << idx << ", res: " << res << endl;
-
-
-        for (int i = 1; i <= 9; ++i) {
-            if (!seen[i]) {
-                if (prev == '#') {
-                    seen[i] = 1;
-                    res.push_back(i + '0');
-                    if (backTrack(idx + 1, s, res, seen, s[idx]))
-                        return true;
-                    res.pop_back();
-                    seen[i] = 0;
-                }
-                else if (prev == 'I') {
-                    if (res.back() - '0' < i) {
-                        res.push_back(i + '0');
-                        seen[i] = 1;
-                        if (backTrack(idx + 1, s, res, seen, s[idx]))
-                            return true;
-                        res.pop_back();
-                        seen[i] = 0;
-                    }
-                }
-                else if (prev == 'D') {
-                    if (res.back() - '0' > i) {
-                        res.push_back(i + '0');
-                        seen[i] = 1;
-                        if (backTrack(idx + 1, s, res, seen, s[idx]))
-                            return true;
-                        res.pop_back();
-                        seen[i] = 0;
-                    }
+        for (int i = 0; i <= pattern.size(); i++) {
+            st.push('1' + i);
+            if (i == pattern.size() || pattern[i] == 'I') {
+                while (!st.empty()) {
+                    result.push_back(st.top());
+                    st.pop();
                 }
             }
         }
-        return false;
-    }
-
-public:
-    string smallestNumber(string pattern) {
-        pattern.push_back('D');
-        string res;
-        vector<int> seen(10, 0);
-        backTrack(0, pattern, res, seen, '#');
-        return res;
+        return result;
     }
 };
