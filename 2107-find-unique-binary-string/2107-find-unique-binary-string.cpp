70d45c695f34efc37b8121934264ae18bb2f463a
Abhishek Gupta <133193177+ashhek@users.noreply.github.com>
2025-02-20 14:51:06 +0530

    Time: 0 ms (100%), Space: 12.6 MB (69.88%) - LeetHub

--- 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
@@ -1,23 +1,15 @@
 class Solution {
-    string res = "";
-    void help(vector<string>&nums, set<string>&st, string temp) {
-        if(temp.size() ==nums.size() && st.find(temp) == st.end()) {
-            res = temp;
-            return;
-        }
-        if(temp.size() > nums.size()) return;
-
-        temp.push_back('1');
-        help(nums, st, temp);
-        temp.pop_back();
-        temp.push_back('0');
-        help(nums, st, temp);
-    }
 public:
     string findDifferentBinaryString(vector<string>& nums) {
-        set<string>st(nums.begin(), nums.end());
-        string temp;
-        help(nums, st, temp);
+        int n = nums.size();
+        string res = "";
+        for(int i=0; i<n; ++i) {
+            if(nums[i][i] == '1') {
+                res += '0';
+            } else {
+                res += '1';
+            }
+        }
         return res;
     }
 };
