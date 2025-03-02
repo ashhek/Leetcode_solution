b0d51f5c28bdd45cb0fd1cff7833f4bf0fe8f5b8
Abhishek Gupta <133193177+ashhek@users.noreply.github.com>
2025-03-02 19:41:19 +0530

    Time: 116 ms (5.76%), Space: 92.3 MB (5.37%) - LeetHub

--- 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.cpp
+++ 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.cpp
@@ -6,28 +6,27 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
-    int cnt = 0;
-    void countGoodNode(TreeNode* root, int mx) {
-        if (!root)
-            return;
-
-        if (root->val >= mx)
-            cnt++;
-
-        mx = max(mx, root->val);
-
-        countGoodNode(root->left, mx);
-        countGoodNode(root->right, mx);
-    }
-
 public:
     int goodNodes(TreeNode* root) {
-        countGoodNode(root, INT_MIN);
+        queue<pair<TreeNode*, int>>q;
+        q.push({root, INT_MIN});
+        int cnt = 0;
+
+        while(!q.empty()) {
+            TreeNode* node = q.front().first;
+            int maxValue = q.front().second;
+            q.pop();
+
+            if(node->val >= maxValue) cnt++;
+            maxValue = max(maxValue, node->val);
+
+            if(node->left) q.push({node->left, maxValue});
+            if(node->right)q.push({node->right, maxValue});
+        }
         return cnt;
     }
 };
